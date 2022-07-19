72fe441c1d0fc6e2a3558101d0660b93bd5392ae
Nyller <edu.sampaio1777@gmail.com>
2022-07-19 18:26:24 -0300

    Commit inicial com regra de negócio definida

--- app/src/main/java/com/nyller/android/conversordemedidas/MainActivity.kt
+++ app/src/main/java/com/nyller/android/conversordemedidas/MainActivity.kt
@@ -2,22 +2,15 @@ package com.nyller.android.conversordemedidas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.nyller.android.conversordemedidas.models.Calculator
-import com.nyller.android.conversordemedidas.models.strategies.CalculationStrategy
-import com.nyller.android.conversordemedidas.models.strategies.KilometerToMeterStrategy
+import com.nyller.android.conversordemedidas.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val calculationStrategy = KilometerToMeterStrategy()
-
-        Calculator.setCalculationStrategy(calculationStrategy)
-
-        Log.i("Edu", "Resultado: ${Calculator.calculate(2.toDouble())}")
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
